fbbbafdd7c2e4e0f418388161a8aa4b27b88c483
Josep R <sony_s07@hotmail.es>
2021-03-01 14:39:29 -0500

    tema 5 > 7/10

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-print('hola mundo');
+const $button = document.querySelector('#tema1');
+const $buttonid = document.getElementById('#tema1');
+
+console.log($button);
+console.log($buttonid);
+// $button.addEventListener('click', click);
+
+function click(evento) {
+  console.log(click);
+}
